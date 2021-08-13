94f387e39b63c7587e48863e6b4e37fd6272a7a6
jkalebe <jeremiaskalebe@gmail.com>
2021-08-13 01:13:14 -0300

    final 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,17 @@
 import './App.css';
 import React from 'react'
-import axios from 'axios'
-import DataTablesComp from './DataTableComp'
-
+import {Route} from 'react-router-dom'
+import Search from './Search'
+import Insert from './Insert';
 
 function App() {
 
-  const [name, setName] = React.useState('')
-  const [data, setData] = React.useState('')
-  const handleChangeInput = e =>{
-    const {value} = e.target
-    setName(value)
-    console.log(value)
-  }
-
-  const searchName = async() =>{
-
-    try {
-      console.log("buscar nome: "+name)
-      await axios.get('http://localhost:5000/user/infor', {headers: {name}})
-        .then((list) =>{
-          setData(list.data.data)
-          console.log(list.data.data)
-        })
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   return (
     <div className="main">
-      <input name="name" onChange={handleChangeInput} placeholder="Seu nome"></input>
-      <button onClick={searchName} >Buscar</button>
-      <div className="output">
-        <DataTablesComp className="datatable" data={data}/>
-      </div>
-
+      <Route exact path="/search" render={props => <Search/>} />
+      <Route exact path="/" render={props => <Search/>} />
+      <Route exact path="/insert" render={props => <Insert/>} />
     </div>
   );
 }
